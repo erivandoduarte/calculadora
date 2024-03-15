50986a259c56b0b99fc4f946785f9d664415ca67
erivando duarte <erivandoduarte01@gmail.com>
2024-03-14 23:28:06 -0300

    Atribuindo Componetes

--- app/src/main/java/com/erivando/calculadora/MainActivity.java
+++ app/src/main/java/com/erivando/calculadora/MainActivity.java
@@ -1,6 +1,9 @@
  package com.erivando.calculadora;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,47 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button numeroZero,numeroUm,numeroDois,numeroTres,numeroQuatro,numeroCinco,numeroSeis,numeroSete,
+            numeroOito,numeroNove,ponto,soma,subtracao,multplicacao,divisao,igual,botaoLimpar;
+
+    private TextView txtEspresao,textResultado;
+
+    private ImageView backspace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        getSupportActionBar().hide();
+
+    };
+
+    private iniciarComponetes(){
+        numeroZero = findViewById(R.id.numeroZero);
+        numeroUm = findViewById(R.id.numero_um);
+        numeroDois = findViewById(R.id.numero_dois);
+        numeroTres = findViewById(R.id.numero_tres);
+        numeroQuatro = findViewById(R.id.numero_quatro);
+        numeroCinco = findViewById(R.id.numero_cinco);
+        numeroSeis = findViewById(R.id.numero_seis);
+        numeroSete = findViewById(R.id.numero_sete);
+        numeroOito = findViewById(R.id.numero_oito);
+        numeroNove = findViewById(R.id.numero_nove);
+        ponto = findViewById(R.id.ponto);
+        soma = findViewById(R.id.soma);
+        subtracao = findViewById(R.id.subtracao);
+        multplicacao = findViewById(R.id.mutiplicacao);
+        divisao = findViewById(R.id.divisao);
+        igual = findViewById(R.id.igual);
+        botaoLimpar =findViewById(R.id.bt_limpar);
+        textResultado = findViewById(R.id.txt_resultado);
+        txtEspresao = findViewById(R.id.txt_expressao);
+        backspace = findViewById(R.id.backspace);
+
     }
+
+
+
 }
